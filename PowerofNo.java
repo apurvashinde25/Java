2358d5231643f51089732430e5bac1c99fd59fa5
Apurva Shinde <130423406+apurvashinde25@users.noreply.github.com>
2023-05-10 00:24:51 +0530

    Add files via upload

---
+++ PowerofNo.java
@@ -0,0 +1,13 @@
+
+public class PowerofNo {
+	public static void main(String args[])
+	{
+		
+		int base_number = 2, exponent = 3;
+		  double pow = Math.pow(base_number, exponent);
+
+		  System.out.println("Result: " + pow);
+		 
+	}
+
+}
